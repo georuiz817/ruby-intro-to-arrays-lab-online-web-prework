0f8a5eb4fa63cc77e2b687851a077b97eab65af6
George Ruiz <georuiz817@gmail.com>
2019-05-21 23:01:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -34,4 +34,6 @@ def first_element_with_array_methods(array)
 end
 
 def last_element_with_array_methods 
-  array = []
+  array = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  array.last
+end

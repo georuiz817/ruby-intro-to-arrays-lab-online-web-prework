f9a87751364ba35cf4e7f93d51f3b5767b58e22d
George Ruiz <georuiz817@gmail.com>
2019-05-21 22:51:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -12,6 +12,6 @@ def array_with_two_elements
 end
 
 def first_element(array)
-array = ["Hello", "this", "is", "the", "argument"]
-array[1]
+array = ["Hello", "Welcome to New York", "this", "is", "the", "argument"]
+array[0]
 end
